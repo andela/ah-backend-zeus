82bf5093138bb187f0a11a46d4dde6983449032d
sekayasin <sekayasin@gmail.com>
2018-11-27 18:37:41 +0300

    chore(postgresql-db): configure postgresqldb engine
    - Configure Authors Haven to use PostgreSQL db Engine
    - Add environment variables
    - Modify .gitignore, Remove migrations
    - git rm --cached files
    
    [Finishes #162163160]

--- authors/settings.py
+++ authors/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7pgozr2jn7zs_o%i8id6=rddie!*0f0qy3$oy$(8231i^4*@u3'
+SECRET_KEY = os.environ.get('PROD_SECRET_KEY','7pgozr2jn7zs_o%i8id6=rddie!*0f0qy3$oy$(8231i^4*@u3')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('PROD_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
@@ -80,10 +81,19 @@ WSGI_APPLICATION = 'authors.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("DB_NAME", 'ah_zeus_db'),
+        'USER': os.environ.get("DB_USER", 'postgres'),
+        'PASSWORD': os.environ.get("DB_PASS", ''),
+        'HOST': os.environ.get("DB_HOST", '127.0.0.1'),
+        'PORT': os.environ.get("DB_PORT", '5432'),
     }
 }
+
+# Heroku $DATABASE_URL - update database configuration
+heroku_db_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(heroku_db_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
