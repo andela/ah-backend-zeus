ed4fb1f60ecc98924e6759af09e85e6972232d25
sekayasin <sekayasin@gmail.com>
2018-11-29 16:29:24 +0300

    chore(deploy_to_heroku): Deploy base application to Heroku
    - Add Procfile
    - Add python3.7 runtime.txt
    - Modify settings
    - update .travis.yml
    
    [Finishes #162163161]

--- authors/settings.py
+++ authors/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('PROD_SECRET_KEY','7pgozr2jn7zs_o%i8id6=rddie!*0f0qy3$oy$(8231i^4*@u3')
+SECRET_KEY = os.environ.get('PROD_SECRET_KEY','7pgozr2jn7zs_o%i8id6=rddie!*0f0qy3$oy$(8231i^4*@u3') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('PROD_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 # Application definition
 
@@ -94,7 +95,6 @@ DATABASES = {
 heroku_db_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(heroku_db_env)
 
-
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
@@ -150,3 +150,6 @@ REST_FRAMEWORK = {
     #     'authors.apps.authentication.backends.JWTAuthentication',
     # ),
 }
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
