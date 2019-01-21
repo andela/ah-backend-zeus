44e2f554c4a7a10c06bd3c7be42fc91571c09f29
Kayabula Alex Joseph <kayabulaalexjoseph@ALEX.local>
2019-01-21 12:22:17 +0300

    fix(social login): fix social login with facebook and google. - fix internal server error 500 when social user tries to login with previously registered email.
    [Finishes #163357225]

--- authors/apps/social_auth_login/register.py
+++ authors/apps/social_auth_login/register.py
@@ -6,45 +6,57 @@ from authors.apps.authentication.renderers import UserJSONRenderer
 
 
 def register_facebook_user(user_id, email, name):
-    user = User.objects.filter(facebook_id=user_id)
-    if not user.exists():
+    filtered_facebook_id = User.objects.filter(facebook_id=user_id)
+    filtered_email = User.objects.filter(email=email)
+
+    if filtered_facebook_id:
+       registered_user = authenticate(email=email, password="XXXXXXXX")
+       return registered_user.token
+
+    elif not filtered_facebook_id and not filtered_email:
         user = {
-            'username': name, 'email': email, 'password': 'XXXXXXXX'}
+           'username': name, 'email': email, 'password': 'XXXXXXXX'}
         User.objects.create_user(**user)
         User.objects.filter(email=email).update(facebook_id=user_id)
         new_user = authenticate(email=email, password="XXXXXXXX")
         return new_user.token
     else:
-        User.objects.filter(facebook_id=user_id)
-        registered_user = authenticate(email=email, password="XXXXXXXX")
-        return registered_user.token
+        return ("A user with this email already exists, Please login")
 
 
 def register_google_user(user_id, email, name):
-    user = User.objects.filter(google_id=user_id)
-    if not user.exists():
-        user = {
-            'username': name, 'email': email, 'password': 'XXXXXXXX'}
+    filtered_google_id=User.objects.filter(google_id=user_id)
+    filtered_email=User.objects.filter(email=email)
+
+    if filtered_google_id:
+       registered_user=authenticate(email=email, password="XXXXXXXX")
+       return registered_user.token
+
+    elif not filtered_google_id and not filtered_email:
+        user={
+           'username': name, 'email': email, 'password': 'XXXXXXXX'}
         User.objects.create_user(**user)
         User.objects.filter(email=email).update(google_id=user_id)
-        new_user = authenticate(email=email, password="XXXXXXXX")
+        new_user=authenticate(email=email, password="XXXXXXXX")
         return new_user.token
     else:
-        User.objects.filter(google_id=user_id)
-        registered_user = authenticate(email=email, password="XXXXXXXX")
-        return registered_user.token
+        return ("A user with this email already exists, Please login")
 
 
 def register_twitter_user(user_id, email, name):
-    user = User.objects.filter(twitter_id=user_id)
-    if not user.exists():
-        user = {
-            'username': name, 'email': email, 'password': 'XXXXXXXX'}
+    filtered_twitter_id=User.objects.filter(twitter_id=user_id)
+    filtered_email=User.objects.filter(email=email)
+
+    if filtered_facebook_id:
+       registered_user=authenticate(email=email, password="XXXXXXXX")
+       return registered_user.token
+
+    elif not filtered_twitter_id and not filtered_email:
+        user={
+           'username': name, 'email': email, 'password': 'XXXXXXXX'}
         User.objects.create_user(**user)
         User.objects.filter(email=email).update(twitter_id=user_id)
-        new_user = authenticate(email=email, password="XXXXXXXX")
+        new_user=authenticate(email=email, password="XXXXXXXX")
         return new_user.token
     else:
-        User.objects.filter(twitter_id=user_id)
-        registered_user = authenticate(email=email, password="XXXXXXXX")
-        return registered_user.token
+        return ("A user with this email already exists, Please login")
